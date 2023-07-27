99cb33a1d8365f363a8b5156e5c0b94d2d6e5407
Hatman00 <2015046@leedstrinity.ac.uk>
2023-07-27 16:47:48 +0100

    Minor change to workload.js

--- static/scripts/workload.js
+++ static/scripts/workload.js
@@ -2,10 +2,9 @@ workloads = document.getElementsByClassName("workload");
 for (let i = 0; i < workloads.length; i++){
     if (parseInt(workloads[i].innerHTML) > 100){
         workloads[i].style.backgroundColor = "rgb(255,0,0)";
-        workloads[i].innerHTML = workloads[i].innerHTML + "%";
     }
     else{
         workloads[i].style.backgroundColor = "rgb(66, 245, 72)";
-        workloads[i].innerHTML = workloads[i].innerHTML + "%";
     }
+    workloads[i].innerHTML = workloads[i].innerHTML + "%";
 }
