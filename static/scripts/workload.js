b30f262fdb4d925f5cfbd079a530592dd38c599f
Hatman00 <2015046@leedstrinity.ac.uk>
2023-07-31 15:26:32 +0100

    Minor change to 'workload.js'

--- static/scripts/workload.js
+++ static/scripts/workload.js
@@ -1,6 +1,6 @@
 workloads = document.getElementsByClassName("workload");
 for (let i = 0; i < workloads.length; i++){
-    if (parseInt(workloads[i].innerHTML) > 100){
+    if (parseInt(workloads[i].innerHTML) >= 95){
         workloads[i].style.backgroundColor = "rgb(255,0,0)";
     }
     else{
@@ -8,3 +8,4 @@ for (let i = 0; i < workloads.length; i++){
     }
     workloads[i].innerHTML = workloads[i].innerHTML + "%";
 }
+//Make this turn yellow if the workload is below 80!
